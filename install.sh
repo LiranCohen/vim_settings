3e2fe9106ef45289d7b0cb16adc2ff86d9ba1486
LIRANCOHEN <lirancohen@me.com>
2015-07-24 12:16:29 -0400

    change cp

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ mkdir -p ~/.vim/autoload &&
 mkdir -p ~/.vim/bundle &&
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cp -rf .vim/bundle/* ~/.vim/bundle/
-cp -rf .vim/autoload/* ~/.vim/autoload/
+cp -rf .vim ~/.vim
 cp -f .vimrc ~/.vimrc
