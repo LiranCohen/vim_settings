04402418931f5d94124af087029c03a620a6140a
LIRANCOHEN <lirancohen@me.com>
2015-07-24 12:15:38 -0400

    restructure woops

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+mkdir -p ~/.vim/autoload &&
+mkdir -p ~/.vim/bundle &&
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-cp -r .vim ~/.vim
-cp .vimrc ~/.vimrc
+cp -rf .vim/bundle/* ~/.vim/bundle/
+cp -rf .vim/autoload/* ~/.vim/autoload/
+cp -f .vimrc ~/.vimrc
