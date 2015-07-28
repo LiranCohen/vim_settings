46e3c1acae8bc62c1e1eef8517cdd5429edb1680
LIRANCOHEN <lirancohen@me.com>
2015-07-27 20:16:56 -0400

    vim-settings

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.vim/autoload &&
 mkdir -p ~/.vim/bundle &&
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim &&
 
-cp -rf .vim ~/.vim
-cp -f .vimrc ~/.vimrc
+cp -rf vim/* ~/.vim/ &&
+cp -f .vimrc ~/.vim/.vimrc &&
+(cd ~/.vim/bundle; git submodule update --init;) &&
+(cd ~/.vim/bundle/YouCompleteMe; ./install.sh --gocode-completer;) &&
+ln -sf ~/.vim/.vimrc /.vimrc
+
+
+
