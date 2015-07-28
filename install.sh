eeec1eede15748472b1350d88eb11c89be4809a4
LIRAN COHEN <c.liran.c@gmail.com>
2015-07-28 16:33:52 -0400

    commit

--- install.sh
+++ install.sh
@@ -4,11 +4,18 @@ mkdir -p ~/.vim/bundle
 rm -rf ~/.vim/bundle/*
 rm -rf ~/.vim/autoload/*
 
+CMAKE=$(cmake --help| grep "command not found" | wc -l)
+until [ $CMAKE -lt 1 ]; do
+    brew install cmake
+    let CMAKE-=1
+done
+
 cd bundle
 git submodule update --init --recursive
 git submodule foreach git pull origin master 
+cd ..
 
-cp -rf bundle/* ~/.vim/bundle
+cp -rf bundle/* ~/.vim/bundle/
 cp -f ~/.vimrc ~/
 
 (cd ~/.vim/bundle/YouCompleteMe; sh install.sh --gocode-completer;)
